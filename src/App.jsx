5ca3b598c1cf86ea93ec97921b80af3681c6e7c0
Zishan <sharkarzishan@gmail.com>
2025-10-18 13:53:21 +0600

    Refactor router implementation to use createHashRouter for improved URL handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import Home from "./pages/Home"
-import NotFound from "./pages/NotFound"
+import { createHashRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 
 function App() {
-  const router = createBrowserRouter([
+  const router = createHashRouter([
     {
       path: "/",
-      element: <Home />
+      element: <Home />,
     },
     {
       path: "*",
-      element: <NotFound />
-    }
-  ])
+      element: <NotFound />,
+    },
+  ]);
 
-  return <RouterProvider router={router} />
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
